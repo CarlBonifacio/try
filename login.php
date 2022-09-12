6bed807f3325eebe2ff8b78d2b0e84496a2dd9c5
CarlBonifacio <113238948+CarlBonifacio@users.noreply.github.com>
2022-09-12 20:10:53 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <!---<title> Responsive Login Form | CodingLab </title>--->
+    <link rel="stylesheet" href="style.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+  	<div class="header">
+  <h1>CITY OF MALABON UNIVERSITY</h1>
+    </div>
+    <div class="container">
+      <form action="#">
+        <div class="title">Online Requisition Login</div>
+        <div class="input-box underline">
+          <input type="text" placeholder="Enter Your Student Number" required>
+          <div class="underline"></div>
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="Enter Your Password" required>
+          <div class="underline"></div>
+        </div>
+        <div class="input-box button">
+          <input type="submit" name="" value="Continue">
+        </div>
+      </form>
+        <div class="option">Forget Password? Click here</div>
+        
+      
+        </div>
+    </div>
+  </body>
+</html>
+
